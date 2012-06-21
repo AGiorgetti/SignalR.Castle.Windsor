c29dd289df5d84b5563ed183b702afe2281f8a0a
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2012-06-21 15:13:08 +0200

    Initial Release
    First commit with a custom dependency resolver and a Hubs installation
    utility class

---
+++ SignalR.Castle.Windsor/CastleWindsorDependencyResolver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.Windsor;
+
+namespace SignalR.Castle.Windsor
+{
+	public class CastleWindsorDependencyResolver : DefaultDependencyResolver
+	{
+		private readonly IWindsorContainer _container;
+
+		public CastleWindsorDependencyResolver(IWindsorContainer container)
+		{
+			if (container == null)
+			{
+				throw new ArgumentNullException("container");
+			}
+
+			_container = container;
+		}
+
+		public override object GetService(Type serviceType)
+		{
+			if (_container.Kernel.HasComponent(serviceType))
+				return _container.Resolve(serviceType);
+			return base.GetService(serviceType);
+		}
+
+		public override IEnumerable<object> GetServices(Type serviceType)
+		{
+			IEnumerable<object> objects;
+			if (_container.Kernel.HasComponent(serviceType))
+				objects = _container.ResolveAll(serviceType).Cast<object>();
+			else
+				objects = new object[] { };
+			return objects.Concat(base.GetServices(serviceType));
+		}
+	}
+}
---
+++ SignalR.Castle.Windsor/HubsInstaller.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Castle.MicroKernel.SubSystems.Configuration;
+using SignalR.Hubs;
+using System.Reflection;
+
+namespace SignalR.Castle.Windsor
+{
+	public class HubsInstallers : IWindsorInstaller
+	{
+		private FromAssemblyDescriptor _descriptor;
+
+		/// <summary>
+		/// registers all the hubs contained in the passed in assembly
+		/// </summary>
+		/// <param name="assembly">The assembly.</param>
+		public HubsInstallers(Assembly assembly)
+		{
+			_descriptor = AllTypes.FromAssembly(assembly);
+		}
+
+		/// <summary>
+		/// registers all the hubs contained in the assembly to which the specified type belongs to
+		/// </summary>
+		/// <param name="typeContainedInAssembly"></param>
+		public HubsInstallers(Type typeContainedInAssembly)
+		{
+			_descriptor = AllTypes.FromAssemblyContaining(typeContainedInAssembly);
+		}
+		
+		/// <summary>
+		/// registers all the hubs in the assemblies specified by the filter
+		/// </summary>
+		/// <param name="filter">The filter.</param>
+		public HubsInstallers(AssemblyFilter filter)
+		{
+			_descriptor = AllTypes.FromAssemblyInDirectory(filter);
+		}
+
+		/// <summary>
+		/// registers all the hubs in the assembly specified by the assembly name parameter
+		/// </summary>
+		/// <param name="assemblyName">Name of the assembly.</param>
+		public HubsInstallers(string assemblyName)
+		{
+			_descriptor = AllTypes.FromAssemblyNamed(assemblyName);
+		}
+
+		public void Install(IWindsorContainer container, IConfigurationStore store)
+		{
+			container.Register(_descriptor.BasedOn(typeof(Hub)));
+		}
+	}
+}
