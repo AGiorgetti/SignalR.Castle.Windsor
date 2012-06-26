08f609f9028d08954c2a4ca948857f7114998fe9
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2012-06-26 15:19:55 +0200

    Improved the DependencyResolver: everything is registered inside castle windsor now.
    Using the full power of the DI container we can dynamically inject
    SignalR logic in all our service classes to realize push notifications
    in an easy way.

--- SignalR.Castle.Windsor/CastleWindsorDependencyResolver.cs
+++ SignalR.Castle.Windsor/CastleWindsorDependencyResolver.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Castle.Windsor;
+using Castle.MicroKernel.Registration;
 
 namespace SignalR.Castle.Windsor
 {
@@ -18,6 +19,12 @@ namespace SignalR.Castle.Windsor
 			}
 
 			_container = container;
+
+			// perform the lazy registrations
+			foreach (var c in _lazyRegistrations)
+				_container.Register(c);
+
+			_lazyRegistrations.Clear();
 		}
 
 		public override object GetService(Type serviceType)
@@ -34,7 +41,49 @@ namespace SignalR.Castle.Windsor
 				objects = _container.ResolveAll(serviceType).Cast<object>();
 			else
 				objects = new object[] { };
-			return objects.Concat(base.GetServices(serviceType));
+
+			var originalContainerServices = base.GetServices(serviceType);
+			if (originalContainerServices != null)
+				return objects.Concat(originalContainerServices);
+
+			return objects;
+		}
+
+		public override void Register(Type serviceType, Func<object> activator)
+		{
+			if (_container != null)
+				// cannot unregister components in windsor, so we use a trick
+				_container.Register(Component.For(serviceType).UsingFactoryMethod<object>(activator, true).OverridesExistingRegistration());
+			else
+				// lazy registration for when the container is up
+				_lazyRegistrations.Add(Component.For(serviceType).UsingFactoryMethod<object>(activator));
+
+			// register the factory method in the default container too
+			//base.Register(serviceType, activator);
+		}
+
+		// a form of laxy initialization is actually needed because the DefaultDependencyResolver starts initializing itself immediately
+		// while we now want to store everything inside CastleWindsor, so the actual registration step have to be postponed until the 
+		// container is available
+		private List<ComponentRegistration<object>> _lazyRegistrations = new List<ComponentRegistration<object>>();
+	}
+
+	public static class WindsorTrickyExtensions
+	{
+		/// <summary>
+		/// Overrideses the existing registration:
+		/// to overide an existiong component registration you need to do two things:
+		/// 1- give it a name.
+		/// 2- set it as default.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="componentRegistration">The component registration.</param>
+		/// <returns></returns>
+		public static ComponentRegistration<T> OverridesExistingRegistration<T>(this ComponentRegistration<T> componentRegistration) where T : class
+		{
+			return componentRegistration
+				.Named(Guid.NewGuid().ToString())
+				.IsDefault();
 		}
 	}
 }
